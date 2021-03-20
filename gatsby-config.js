5284bff0f8779a38f74430cb58de0cbe8e625700
Joshua Kemmerling <joshua.kemmerling@users.noreply.github.com>
2021-03-19 19:05:55 -0500

    Intial files for example site

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+      {
+        resolve: 'gatsby-theme-app-landing-page',
+        options: {
+          appStoreId: '953286746',
+        },
+      },
+    ],
+  };
+  
